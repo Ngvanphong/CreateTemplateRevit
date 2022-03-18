db119ae5182e0d4d2bd2e9e3b76054451ba8a3a5
Nguyen van Phong <ngvanphong2012@gmail.com>
2022-03-18 10:36:37 +0700

    Add project files.

---
+++ CreateTemplateRevit/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+namespace CreateTemplateRevit
+{
+    internal class App:IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
